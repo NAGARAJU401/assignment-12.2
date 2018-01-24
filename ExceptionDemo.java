374a968a403bd93402b1af7f7244c1dfd52f49be
NAGARAJU401 <31498449+NAGARAJU401@users.noreply.github.com>
2018-01-24 16:02:31 +0530

    Add files via upload

---
+++ ExceptionDemo.java
@@ -0,0 +1,105 @@
+package exception;
+import java.util.Scanner;
+public class ExceptionDemo {
+
+	public static void main(String[] args) {
+		//Public is a keyword that is used as an access modifier for methods and variables.
+				//static used to prepare a field,method or inner classes as a class field.
+				//void: it is void if the method does not return a value.
+				//main:it is a function name When a program starts running, it has to start execution from somewhere.
+				//String[] args: it is  defining a String array to pass arguments at command line. args is the variable name of the String array.
+				
+		// TODO Auto-generated method stub
+		System.out.println("Please enter 5 string values in the string array.");
+		//System.out.println is a Java statement that prints the argument passed, into the System.out which is generally stdout.
+		        
+
+		        String[] arr = new String[5]; // Initializing the string array of size 5
+
+		        Scanner sc = new Scanner(System.in); // Scanner object to get the user
+
+		                                                // input
+
+		        for (int i = 0; i < arr.length; i++) { // Getting input from the user
+
+		                                                // Assuming only one word will
+
+		                                                // be entered
+		//here it will print the one word and once the word is printed then it will increment to next one
+		            System.out.print("String #" + (i + 1) + ": ");
+		//here it will pont the scanner to take next element in the array
+		            arr[i] = sc.next();
+
+		        }
+
+		        String str;//initializing a string datatype
+
+		        System.out.print("\nEnter the string to search: ");
+
+		        str = sc.next(); // Getting the search string from user
+
+		        String str1 = searchString(arr, str); // calling the method to search
+
+		                                                // string in array
+
+		        if (str1 == null) { // If method returned null, that means string not
+
+		                            // found
+
+		            System.out.println("String not found.");
+
+		        } else { // else string found and result printed accordingly
+
+		            System.out.println("String found.");
+
+		        }
+
+		    }
+
+		    // method to search the string
+
+		    private static String searchString(String[] arr, String str) {
+		 //Flow of try catch block.,If an exception occurs in try block then the control of execution
+		 //is passed to the catch block from try block. The exception is caught up by the corresponding catch block
+		        try {
+
+		            int i;//initializing the int data type
+
+		            for (i = 0; i < arr.length; i++) { // for loop to search the string
+
+		                                                // in array
+
+		                if (arr[i].equals(str)) { // If string found stop the loop
+
+		                    break;
+
+		                }
+
+		            }
+
+		            if (i == arr.length) { // If value of i is equal to array length ,
+
+		                                    // it means, whole array traversed but
+
+		                                    // string not found
+
+		                throw new ValueNotFoundException();// So exception is thrown
+
+		            } else {
+
+		                return str; // if i is less than array length, it means value
+
+		                            // found , so returned the string itself
+
+		            }
+
+		        } catch (ValueNotFoundException e) {
+
+		            return null; // If exception caught then null is returned
+
+		        }
+
+
+	}
+
+}
---
+++ ValueNotFoundException.java
@@ -0,0 +1,14 @@
+package exception;
+
+public class ValueNotFoundException extends Exception {
+	//public : members which can access as public,public members are visible to all other classes.
+			//class : is a context of java that are used to create objects and to define object data types and methods.
+		
+	//when the value is not found then we will use this exception	
+		public ValueNotFoundException(){
+//The super keyword in java is a reference variable that is used to refer immediate parent class object
+			super();
+
+		}
+
+}
